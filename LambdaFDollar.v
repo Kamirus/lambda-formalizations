f1be3f6f64cb0e98587e0d30347f2a650fcbeb82
Kamirus <listopadkamil@gmail.com>
2022-02-13 12:14:13 +0100

    my lambda dollar decompose inversion lemmas

--- LambdaFDollar.v
+++ LambdaFDollar.v
@@ -49,8 +49,8 @@ Section Contexts.
   Fixpoint plugT' trail e :=
     match trail with
     | T_nil' => e
-    (* | T_cons v k t => plugT t <{ {val_to_tm v} $ {plugK k e} }> *)
-    | T_cons' v t => <{ {val_to_tm v} $ {plugT' t e} }>
+    | T_cons' v t => plugT' t <{ {val_to_tm v} $ e }>
+    (* | T_cons' v t => <{ {val_to_tm v} $ {plugT' t e} }> *)
     end.
 
   Definition redex_to_term' r := 
@@ -109,12 +109,97 @@ Fixpoint decompose' (e : tm ␀) : dec' ␀ :=
     | dec_stuck' k e'  => dec_stuck' (K_dol k e2) e'
     | dec_redex' k t r => dec_redex' (K_dol k e2) t r
     | dec_value' v1    => 
-      match decompose'T T_nil' v1 e2 with
-      | pair t r => dec_redex' K_nil t r
-      end
+      let (t, r) := decompose'T T_nil' v1 e2 in
+      dec_redex' K_nil t r
     end
   end.
 
+Ltac inv_decompose_match' H :=
+  match goal with
+  | H : (match decompose' ?e with | dec_stuck' _ _ | dec_redex' _ _ _ | dec_value' _ => _ end = _) |- _ =>
+    let d := fresh "d" in remember (decompose' e) as d; inv d; inv_decompose_match' H
+  | H : (let (_,_) := decompose'T ?t ?w ?e in _) = _ |- _ =>
+    let d := fresh "d" in remember (decompose'T t w e) as d; inv d; inv_decompose_match' H
+  | _ => try solve [inversion H; auto]; try inj H
+  end.
+
+(* plug ∘ decompose' = id *)
+Lemma decompose_value_inversion' : ∀ e v,
+  decompose' e = dec_value' v → e = val_to_tm v.
+Proof.
+  intros. inv e; cbn in *.
+  - destruct a.
+  - inj H; auto.
+  - inversion H.
+  - inv_decompose_match' H.
+  - inv_decompose_match' H.
+  Qed.
+
+Lemma decompose_stuck_inversion' : ∀ e k e',
+  decompose' e = dec_stuck' k e' → e = plugK k <{ S₀ e' }>.
+Proof.
+  intros e k; generalize dependent e;
+  induction k; intros; inv e; cbn in *;
+  try solve [inversion a | inversion H | inj H; reflexivity];
+  inv_decompose_match' H; cbn;
+  try rewrite (decompose_value_inversion' e1 v); cbn; auto;
+  f_equal; apply IHk; auto.
+  Qed.
+
+Ltac inv_dec' :=
+  match goal with
+  | H : dec_value' ?v = decompose' ?e |- _ => rewrite (decompose_value_inversion' e v); cbn; auto
+  | H : dec_stuck' ?k ?e' = decompose' ?e |- _ => rewrite (decompose_stuck_inversion' e k e'); cbn; auto
+  end.
+
+Lemma tm_decide : ∀ e, (∃ v, tm_dec e = inl v) \/ (∃ p, tm_dec e = inr p).
+Proof.
+  intro e; remember (tm_dec e) as d eqn: H; destruct d; solve [left + right; eexists; auto].
+  Qed.
+
+Lemma tm_dec_val : ∀ e v, tm_dec e = inl v → e = val_to_tm v.
+Proof.
+  intros; inv e; cbn in *; try destruct a; inv H; auto.
+  Qed.
+Lemma tm_dec_non : ∀ e p, tm_dec e = inr p → e = non_to_tm p.
+Proof.
+  intros; inv e; cbn in *; try destruct a; inv H; auto.
+  Qed.
+
+Ltac inv_tm_dec :=
+  match goal with
+  | H : (_, _) = (_, _) |- _ => inj H
+  | H : ␀ |- _ => destruct H
+  | H : inl ?v = tm_dec ?e |- _ => rewrite (tm_dec_val e v (eq_sym H)) in *; auto
+  | H : inr ?p = tm_dec ?e |- _ => rewrite (tm_dec_non e p (eq_sym H)) in *; auto
+  | H : context C [match tm_dec ?e with inl _ => _ | inr _ => _ end] |- _ =>
+    let td := fresh "td" in
+    let Hd := fresh "Hd" in
+    remember (tm_dec e) as td eqn: Hd; inv td; try rewrite Hd in *
+  end.
+
+Lemma decomposeT_inversion' : ∀ t v e t' r,
+  decompose'T t v e = (t', r) → 
+  plugT' t <{ {val_to_tm v} $ e }> = plugT' t' (redex_to_term' r).
+Proof.
+  intros t v e; generalize dependent v; generalize dependent t;
+  dependent induction e; intros; cbn in *;
+  repeat inv_tm_dec; cbn;
+  try solve [f_equal; auto].
+  apply IHe2 in H; auto.
+  Qed.
+
+Lemma decompose_redex_inversion' : ∀ e t k r,
+  decompose' e = dec_redex' k t r → e = plugK k (plugT' t (redex_to_term' r)).
+Proof.
+  dependent induction e; intros; cbn in *;
+  try solve [destruct a | inv H];
+  inv_decompose_match' H;
+  repeat inv_dec'; cbn; f_equal;
+  try solve [apply IHk + apply IHe1 + apply IHe2; auto].
+  erewrite <- (decomposeT_inversion' _ _ _ t r); eauto.
+  auto.
+  Qed.
 
 
 (* v $ K[p] ~> v' $ p *)
